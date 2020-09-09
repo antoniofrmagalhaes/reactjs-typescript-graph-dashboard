d38fb1a28f5f32be42cbe9f168aef11e756131fb
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-09 00:27:38 -0400

    feat: initial global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  #root {
+    background: #F5F5FB;
+  }
+
+`;
