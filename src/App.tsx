9eb36c1d1915b86bf0efb59fab64168db446aa12
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-09 00:29:08 -0400

    feat: global styles added on main app

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
 
+import GlobalStyles from "./styles/global";
+
 function App() {
   return (
     <Router>
       <Routes />
+      <GlobalStyles />
     </Router>
   );
 }
