82bf9dce33f90c51e0ddaa17e6f3e615ea1e86d9
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2020-09-11 12:30:59 -0400

    test: add some routes for tests

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,11 @@ import Route from "../routes/DashboardRoute";
 const Routes: React.FC = () => {
   return (
     <Switch>
-      <Route path="/" exact component={() => <h1>Dashboard</h1>} />
+      <Route path="/" exact component={() => <h1>dashboard</h1>} />
+      <Route path="/informations" component={() => <h1>informations</h1>} />
+      <Route path="/desktops" component={() => <h1>desktops</h1>} />
+      <Route path="/orders" component={() => <h1>orders</h1>} />
+      <Route path="/settings" component={() => <h1>settings</h1>} />
     </Switch>
   );
 };
